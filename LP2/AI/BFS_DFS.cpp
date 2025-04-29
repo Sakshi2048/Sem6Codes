f82796fb3dc9e5c1bd9a36c98cb0db4332b24fc5
Sakshi Marbhal <sakshimarbhal248@gmail.com>
2025-04-29 14:57:09 +0530

    LP2 folder created

---
+++ LP2/AI/BFS_DFS.cpp
@@ -0,0 +1,186 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <stack>
+using namespace std;
+
+class Graph
+{
+    int V;
+    vector<vector<int>> adj;
+
+public:
+    Graph(int x)
+    {
+        V = x;
+        adj = vector<vector<int>>(V);
+    }
+
+    void addEdge(int u, int v)
+    {
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+
+    void BFS(int s, int x)
+    {
+        bool ans = false;
+        if (s == x)
+        {
+            cout << "Found! Start Node = Search Node" << endl;
+            return;
+        }
+
+        vector<bool> visited(V, false);
+        queue<int> q;
+        q.push(s);
+        visited[s] = true;
+
+        while (!q.empty())
+        {
+            int a = q.front();
+            if (a == x)
+            {
+                ans = true;
+            }
+            cout << a << " ";
+            q.pop();
+
+            for (int i = 0; i < adj[a].size(); i++)
+            {
+                int m = adj[a][i];
+                if (!visited[m])
+                {
+                    visited[m] = true;
+                    q.push(m);
+                }
+            }
+        }
+        if (ans)
+        {
+            cout << endl;
+            cout << "Found!" << endl;
+        }
+        else
+        {
+            cout << endl;
+            cout << "Not Found!" << endl;
+        }
+    }
+
+    void DFS(int s, int x)
+    {
+        bool ans = false;
+        if (s == x)
+        {
+            cout << "Found! Start Node = Search Node" << endl;
+            return;
+        }
+
+        vector<bool> visited(V, false);
+        stack<int> st;
+        st.push(s);
+        visited[s] = true;
+
+        while (!st.empty())
+        {
+            int a = st.top();
+            if (a == x)
+            {
+                ans = true;
+            }
+            cout << a << " ";
+            st.pop();
+
+            for (int i = 0; i < adj[a].size(); i++)
+            {
+                int m = adj[a][i];
+                if (!visited[m])
+                {
+                    visited[m] = true;
+                    st.push(m);
+                }
+            }
+        }
+        if (ans)
+        {
+            cout << endl;
+            cout << "Found!" << endl;
+        }
+        else
+        {
+            cout << endl;
+            cout << "Not Found!" << endl;
+        }
+    }
+
+    void DFSUtil(int s, int x, vector<bool> &visited, bool &found)
+    {
+        visited[s] = true;
+        cout << s << " ";
+        if (s == x)
+        {
+            found = true;
+            return; // Early return if found
+        }
+
+        for (int i = 0; i < adj[s].size(); i++)
+        {
+            int v = adj[s][i];
+            if (!visited[v])
+            {
+                DFSUtil(v, x, visited, found);
+                if (found)
+                    return; // Stop further recursion once found
+            }
+        }
+    }
+
+    void DFSR(int s, int x)
+    {
+        bool found = false;
+        vector<bool> visited(V, false);
+        DFSUtil(s, x, visited, found);
+        cout << endl;
+        if (found)
+        {
+            cout << "Found!" << endl;
+        }
+        else
+        {
+            cout << "Not Found!" << endl;
+        }
+    }
+};
+int main()
+{
+
+    int V, E;
+    cout << "Enter number of vertices: ";
+    cin >> V;
+    cout << "Enter number of edges: ";
+    cin >> E;
+
+    Graph g(V);
+
+    cout << "Enter edges: " << endl;
+    for (int i = 0; i < E; i++)
+    {
+        int u, v;
+        cin >> u >> v;
+        g.addEdge(u, v);
+    }
+
+    int s, x;
+    cout << "Enter starting node for traversal: ";
+    cin >> s;
+    cout << "Enter node to search (x): ";
+    cin >> x;
+
+    g.BFS(s, x);
+    g.DFS(s, x);
+    g.DFSR(s, x);
+
+    return 0;
+}
---
+++ LP2/AI/NQueens.cpp
@@ -0,0 +1,109 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void printBoard(vector<vector<int>> &matrix)
+{
+    int n = matrix.size();
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << (matrix[i][j] == 1 ? "1 " : "0 ");
+        }
+        cout << endl;
+    }
+}
+
+bool isSafe(vector<vector<int>> &matrix, int row, int col)
+{
+    int n = matrix.size();
+
+    // Check column
+    for (int i = 0; i < row; i++)
+    {
+        if (matrix[i][col] == 1)
+            return false;
+    }
+
+    // Check upper left diagonal
+    for (int i = row, j = col; i >= 0 && j >= 0; i--, j--)
+    {
+        if (matrix[i][j] == 1)
+            return false;
+    }
+
+    // Check upper right diagonal
+    for (int i = row, j = col; i >= 0 && j < n; i--, j++)
+    {
+        if (matrix[i][j] == 1)
+            return false;
+    }
+
+    return true;
+}
+
+void placeQueen(vector<vector<int>> &matrix, int row, int &solutionCount)
+{
+
+    int n = matrix.size();
+
+    // All Queens placed - solution found
+    if (row == n)
+    {
+        solutionCount++;
+        cout << "Solution " << solutionCount << ":\n";
+        printBoard(matrix);
+        cout << endl;
+        return;
+    }
+
+
+    for (int col = 0; col < n; col++)
+    {
+        // If Cell is safe to place
+        if (isSafe(matrix, row, col))
+        {
+            // Place Queen
+            matrix[row][col] = 1;
+
+            // Place next queens
+            placeQueen(matrix, row + 1, solutionCount);
+
+            // Backtracking - if no safe place found for a queen - remove current queen
+            matrix[row][col] = 0; 
+        }
+    }
+}
+
+void solve(int n)
+{
+    // Initialize board
+    vector<vector<int>> matrix(n, vector<int>(n, 0));
+
+    // Possible no. of solutions
+    int solutionCount = 0;
+
+    placeQueen(matrix, 0, solutionCount);
+
+    // If no solution exist
+    if (solutionCount == 0)
+    {
+        cout << "No solution exists!" << endl;
+    }
+}
+
+int main()
+{
+
+    int n;
+    cout << "Total Queens: ";
+    cin >> n;
+
+    cout << endl;
+
+    solve(n);
+    
+    return 0;
+}
