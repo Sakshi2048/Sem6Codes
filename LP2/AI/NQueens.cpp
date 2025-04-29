e9dac9ca896cea094b2fa84bd33bc83615b66201
Sakshi Marbhal <sakshimarbhal248@gmail.com>
2025-04-29 15:40:53 +0530

    n queens perfect

--- LP2/AI/NQueens.cpp
+++ LP2/AI/NQueens.cpp
@@ -3,58 +3,58 @@
 
 using namespace std;
 
-void printBoard(vector<vector<int>> &matrix)
+void printBoard(vector<vector<int>> &board)
 {
-    int n = matrix.size();
+    int n = board.size();
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < n; j++)
         {
-            cout << (matrix[i][j] == 1 ? "1 " : "0 ");
+            cout << (board[i][j] == 1 ? "Q " : "- ");
         }
         cout << endl;
     }
 }
 
-bool isSafe(vector<vector<int>> &matrix, int row, int col)
+bool isSafe(vector<vector<int>> &board, int row, int col)
 {
-    int n = matrix.size();
+    int n = board.size();
 
     // Check column
     for (int i = 0; i < row; i++)
     {
-        if (matrix[i][col] == 1)
+        if (board[i][col] == 1)
             return false;
     }
 
     // Check upper left diagonal
     for (int i = row, j = col; i >= 0 && j >= 0; i--, j--)
     {
-        if (matrix[i][j] == 1)
+        if (board[i][j] == 1)
             return false;
     }
 
     // Check upper right diagonal
     for (int i = row, j = col; i >= 0 && j < n; i--, j++)
     {
-        if (matrix[i][j] == 1)
+        if (board[i][j] == 1)
             return false;
     }
 
     return true;
 }
 
-void placeQueen(vector<vector<int>> &matrix, int row, int &solutionCount)
+void placeQueen(vector<vector<int>> &board, int row, int &solutionCount)
 {
 
-    int n = matrix.size();
+    int n = board.size();
 
     // All Queens placed - solution found
     if (row == n)
     {
         solutionCount++;
         cout << "Solution " << solutionCount << ":\n";
-        printBoard(matrix);
+        printBoard(board);
         cout << endl;
         return;
     }
@@ -63,16 +63,16 @@ void placeQueen(vector<vector<int>> &matrix, int row, int &solutionCount)
     for (int col = 0; col < n; col++)
     {
         // If Cell is safe to place
-        if (isSafe(matrix, row, col))
+        if (isSafe(board, row, col))
         {
             // Place Queen
-            matrix[row][col] = 1;
+            board[row][col] = 1;
 
             // Place next queens
-            placeQueen(matrix, row + 1, solutionCount);
+            placeQueen(board, row + 1, solutionCount);
 
             // Backtracking - if no safe place found for a queen - remove current queen
-            matrix[row][col] = 0; 
+            board[row][col] = 0; 
         }
     }
 }
@@ -80,12 +80,12 @@ void placeQueen(vector<vector<int>> &matrix, int row, int &solutionCount)
 void solve(int n)
 {
     // Initialize board
-    vector<vector<int>> matrix(n, vector<int>(n, 0));
+    vector<vector<int>> board(n, vector<int>(n, 0));
 
     // Possible no. of solutions
     int solutionCount = 0;
 
-    placeQueen(matrix, 0, solutionCount);
+    placeQueen(board, 0, solutionCount);
 
     // If no solution exist
     if (solutionCount == 0)
